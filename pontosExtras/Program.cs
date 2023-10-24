f1c398c84dc50b82ce6fc7ebbf3f1bea3c347a23
Enzo França <Enzofran4@gmail.com>
2023-10-24 13:25:04 -0400

    terceiro commit

--- pontosExtras/Program.cs
+++ pontosExtras/Program.cs
@@ -8,9 +8,7 @@ namespace pontosExtras
 {
     internal static class Program
     {
-        /// <summary>
-        /// Ponto de entrada principal para o aplicativo.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
